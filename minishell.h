864f187e59d101e5a48b8c236c5110ddfb61d69c
ayats37 <taya@student.1337.ma>
2025-06-22 12:48:43 +0100

    dwfer

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: taya <taya@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 12:58:09 by ouel-afi          #+#    #+#             */
-/*   Updated: 2025/06/22 12:03:16 by taya             ###   ########.fr       */
+/*   Updated: 2025/06/22 12:47:52 by taya             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,61 +109,70 @@ t_env *init_env(char **env);
 t_env *create_env_node(char *env_var);
 void add_to_env_list(t_env **head, t_env *new_node);
 t_env *init_env(char **envp);
+char **env_list_to_array(t_env *env_list);
+char	*build_env_string(char *name, char *value);
+t_env *find_env_var(char *name, t_env *env_list);
+void env_append(char *name, char *value, t_env **env_list);
+int count_env_nodes(t_env *env_list);
+
 int ft_echo(char **cmd);
+int is_valid_n_flag(char *arg);
+void	update_pwd_vars(char *oldpwd, t_env *envlist);
 int ft_pwd();
+char	*get_cd_path(char **cmd, char *oldpwd);
+char	*get_home_path(char **cmd, char *oldpwd);
+char	*get_oldpwd_path(char **cmd, char *oldpwd);
+char	*get_envvar_path(char **cmd, char *oldpwd);
 int ft_cd(char **cmd, t_env *envlist);
+void	unset_var(t_env **env_list, char *name);
 int ft_unset(char **cmd, t_env **env_list);
+int	check_exit_args(char **cmd);
 int ft_exit(char **cmd, t_env *env_list);
 int ft_env(t_env **env_list);
+int	valid_identifier(char *str);
+int	parse_export_arg(char *arg, char **name, char **value, int *append);
+int	process_export(char *arg, t_env **env_list);
 int ft_export(char **cmd, t_env **env_list);
-int execute_builtin(t_tree *node, t_env **envlist);
-int  is_alphanumeric(int c);
-char	*find_cmd_path(char *cmd, t_env **envlist);
-char	**ft_split(char const *s, char c);
-int execute_tree(t_tree *node, t_env **envlistm, int last_status);
-int execute_cmd(char **cmds, t_env *envlist, t_tree *node);
-int is_builtin(char *cmd);
-int handle_redirection(t_tree *node);
-int  is_alpha(int c);
-void env_append(char *name, char *value, t_env **env_list);
-t_env *find_env_var(char *name, t_env *env_list);
-int execute_pipe(t_tree *node, t_env **envlist, int last_status);
-void handle_heredoc_input(char *delimiter, int write_fd);
-// void process_heredocs_tree(t_tree *node);
-int is_valid_n_flag(char *arg);
-void write_error(char *command, char *message);
-void handler(int sig);
+
 void free_tokens(t_token *tokens);
 void free_tree(t_tree *node);
 void free_lexer(t_lexer *lexer);
 void free_resources(char *input, t_lexer *lexer, t_token *token_list, t_tree *node);
-void setup_shell_terminal(void);
-void heredoc_sigint_handler(int sig);
+void	free_env_list(t_env *env);
+void	free_env_array_partial(char **env_array, int i);
+void free_env_array(char **env_array);
+void	ft_free_arr(char **arr);
+
+int  is_alphanumeric(int c);
+int  is_alpha(int c);
 int is_num(const char *str);
 int is_digit(int c);
-char *expand_token(char *token, t_env *env_list, int last_exit_status);
-char *str_join_free(char *s1, const char *s2);
-void expand_variables(char **tokens, int last_exit_status, t_env *env_list);
-char *char_to_str(char c);
-void free_env_array(char **env_array);
-char **env_list_to_array(t_env *env_list);
-int count_env_nodes(t_env *env_list);
-int process_heredocs_tree(t_tree *node);
-void	free_env_list(t_env *env);
-int	check_exit_args(char **cmd);
-char	*build_env_string(char *name, char *value);
-void	free_env_array_partial(char **env_array, int i);
-void	unset_var(t_env **env_list, char *name);
-int	parse_export_arg(char *arg, char **name, char **value, int *append);
+
 char	*get_variable_value(char *token, int *i, t_env *env_list);
 char	*expand_dollar_var(char *token, int *i, t_env *env_list, int last_exit_status);
 int	append_char_to_result(char **result, char c);
 char	*expand_token(char *token, t_env *env_list, int last_exit_status);
-void	ft_free_arr(char **arr);
-char	*get_cd_path(char **cmd, char *oldpwd);
-void	update_pwd_vars(char *oldpwd, t_env *envlist);
-char	*get_home_path(char **cmd, char *oldpwd);
-char	*get_oldpwd_path(char **cmd, char *oldpwd);
-char	*get_envvar_path(char **cmd, char *oldpwd);
+void expand_variables(char **tokens, int last_exit_status, t_env *env_list);
+
+char	*find_cmd_path(char *cmd, t_env **envlist);
+char	*check_paths(char **paths, char *cmd);
+char	*build_path(char *path, char *cmd);
+char	**get_paths(t_env **envlist);
+char	*get_path(t_env *envlist);
+
+int is_builtin(char *cmd);
+int execute_builtin(t_tree *node, t_env **envlist);
+int execute_tree(t_tree *node, t_env **envlistm, int last_status);
+int execute_cmd(char **cmds, t_env *envlist, t_tree *node);
+int handle_redirection(t_tree *node);
+int execute_pipe(t_tree *node, t_env **envlist, int last_status);
+void handle_heredoc_input(char *delimiter, int write_fd);
+void write_error(char *command, char *message);
+void handler(int sig);
+void setup_shell_terminal(void);
+void heredoc_sigint_handler(int sig);
+char *str_join_free(char *s1, const char *s2);
+char *char_to_str(char c);
+int process_heredocs_tree(t_tree *node);
 
 #endif
